5e8c026ce48eefe0af871be9ee332967b49a9d14
Shubham Verma <subham.varma@reachmobile.com>
2024-03-03 19:53:07 +0530

    added user and video models

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 // require('dotenv').config()
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import { app } from "./app.js";
 
 dotenv.config({
-  path: "./env"
-})
-const PORT = process.env.PORT || 8000
-connectDB().then(()=> {
-  app.listen(PORT, ()=> {
-    console.log(`Server is running on port: ${PORT}`)
+  path: "./env",
+});
+
+const PORT = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
   })
-}).catch(err=> console.log("MongoDB connection failed !!!",err))
-
-
-
+  .catch((err) => console.log("MongoDB connection failed !!!", err));
 
 /*
 import express from 'express'
